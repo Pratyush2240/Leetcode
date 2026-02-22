e01ba660d4e069e7721abaad0d0118e51527ed98
Pratyush Kumar <prtsh2005@gmail.com>
2026-02-22 21:21:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (84.49%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+        int lastPos = -1;
+        int maxDist = 0;
+        int position = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (lastPos != -1) {
+                    maxDist = Math.max(maxDist, position - lastPos);
+                }
+                lastPos = position;
+            }
+            n >>= 1;
+            position++;
+        }
+
+        return maxDist;
+    }
+}
